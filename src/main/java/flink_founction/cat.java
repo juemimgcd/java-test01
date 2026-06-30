97628b9159f5deb1b3ca6704345ec68d4997ce24
juemimgcd <3489161689@qq.com>
2026-06-30 23:06:19 +0800

    start

---
+++ src/main/java/flink_founction/cat.java
@@ -0,0 +1,38 @@
+package flink_founction;
+
+public class cat {
+    int age;
+    String name;
+
+    public cat() {
+    }
+
+    public cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "cat{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
