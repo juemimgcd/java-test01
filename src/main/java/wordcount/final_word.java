212100d676d1110402eb0134267770cfb8d070a5
juemimgcd <3489161689@qq.com>
2026-07-03 21:57:05 +0800

    start

---
+++ src/main/java/wordcount/final_word.java
@@ -0,0 +1,45 @@
+package wordcount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class final_word {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(2);
+        env.readTextFile("E:\\java_play\\flink\\src\\main\\java\\wordcount\\word.txt").flatMap(new FlatMapFunction<String, Tuple2<String,Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] words = value.split(",");
+                for (String word : words) {
+                    out.collect(Tuple2.of(word,1));
+                }
+            }
+        })
+                .keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Integer> value) throws Exception {
+                        return value.f0;
+                    }
+                })
+                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+                    @Override
+                    public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
+                        return Tuple2.of(value1.f0,value1.f1 + value2.f1);
+                    }
+                })
+                .rescale()
+                .print().setParallelism(4);
+        env.execute();
+
+
+
+    }
+
+
+
+}
